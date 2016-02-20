527fdfb67918083fa8865d2c34c4eca3854cc8ef
Brian Isganitis <brianji03@gmail.com>
2016-02-20 16:24:16 -0500

    More string functions: variables, operators, values.

--- src/to_string.ml
+++ src/to_string.ml
@@ -9,5 +9,23 @@ let return_string = function
   | Void -> "void"
   | Prim p -> prim_string p
 
-let param_string = function
-  (prim, id) -> (prim_string prim) ^ " " ^ id
+let param_string = function (prim, id) -> (prim_string prim) ^ " " ^ id
+
+let var_string = function (id, _) -> id
+
+let inop_string = function
+  | Plus -> "+"
+  | Minus -> "-"
+  | Times -> "*"
+  | Divide -> "/"
+  | Mod -> "%"
+  | Asgmt -> "="
+
+let endop_string = function
+  | Incrmt -> "++"
+  | Decrmt -> "--"
+
+let value_string = function
+  | Integer i -> string_of_int i
+  | Decimal d -> string_of_float d
+  | Letter l -> String.make 1 l
