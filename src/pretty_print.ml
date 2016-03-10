007e951658777d25500c867d100a7d98f27243a7
Brian Isganitis <brianji03@gmail.com>
2016-03-10 15:53:29 -0500

    Support for declarations, initializations. Fixed parameters.

--- src/pretty_print.ml
+++ src/pretty_print.ml
@@ -4,47 +4,33 @@ open To_string
 let tail l = try List.tl l with Failure _ -> []
 
 let print_param (prim, id) =
-    print_string (prim_string prim);
-    print_string " ";
-    print_string id
+  print_string (prim_string prim);
+  print_string " ";
+  print_string id
 
 let print_params params =
   let aux a p =
     print_param p;
-    tail a
+    match a with
+      | [] -> []
+      | _ :: t ->
+        print_string ", ";
+        t
   in
   let _ = List.fold_left aux (tail params) params in ()
-
-let print_dec_expr = function
-  | DecVar id -> print_string id
-  | InitVar (id, expr) ->
-    print_string id;
-    print_string " = ";
-    print_string "TODO"
-
-let print_dec (p, l) =
-  print_string (prim_string p);
-  print_string " ";
-  let aux a dec =
-    print_dec_expr dec;
-    tail a
-  in
-  let _ = List.fold_left aux (tail l) l in
-  print_string ";";
-  print_newline ()
 
 let print_inop i =
   print_string " ";
   print_string (inop_string i);
   print_string " "
 
-let rec print_exp = function
+let rec print_expr = function
   | Var v -> print_string v
   | Value v -> print_string (value_string v)
   | Infix (e1, i, e2) ->
-    print_exp e1;
+    print_expr e1;
     print_inop i;
-    print_exp e2
+    print_expr e2
   | Prefix (e, id) ->
     print_string (endop_string e);
     print_string id
@@ -52,10 +38,32 @@ let rec print_exp = function
     print_string id;
     print_string (endop_string e)
 
+let print_dec_expr = function
+  | DecVar id -> print_string id
+  | InitVar (id, expr) ->
+    print_string id;
+    print_string " = ";
+    print_expr expr
+
+let print_dec (p, l) =
+  print_string (prim_string p);
+  print_string " ";
+  let aux a dec =
+    print_dec_expr dec;
+    match a with
+      | [] -> []
+      | _ :: t ->
+        print_string ", ";
+        t
+  in
+  let _ = List.fold_left aux (tail l) l in
+  print_string ";";
+  print_newline ()
+
 let print_statement = function
   | Dec (p, l) -> print_dec (p, l)
   | Expr e ->
-    print_exp e;
+    print_expr e;
     print_string ";";
     print_newline ()
 
