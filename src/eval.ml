553b3111bbb6240cec9acb08b8cd4ccd8f11994d
Brian Isganitis <brianji03@gmail.com>
2016-04-09 19:32:06 -0400

    Support for int and void functions.

--- src/eval.ml
+++ src/eval.ml
@@ -5,7 +5,7 @@ type expr_result =
   | IntRes of int
 and statement_result =
   | NoRes
-  | RetRes of int
+  | RetRes of expr_result
   | BrkRes
   | ContRes
 
@@ -16,14 +16,17 @@ let int_of_bool b = if b then 1 else 0
 
 let rec eval_expr expr fcns scope = match expr with
   | Empty -> failwith "Empty expression." (* TODO: for loop empty expression *)
-  | Var v -> eval_var v fcns scope
+  | Var v -> IntRes (eval_var v fcns scope)
   | Value v -> eval_value v
   | Paren e -> eval_expr e fcns scope
   | FunctionCall f -> eval_function_call f fcns scope
-  | Infix i -> eval_infix i fcns scope
-  | Assign a -> eval_assign a fcns scope
-  | Prefix p -> eval_prefix p fcns scope
-  | Postfix p -> eval_postfix p fcns scope
+  | Infix i -> IntRes (eval_infix i fcns scope)
+  | Assign a -> IntRes (eval_assign a fcns scope)
+  | Prefix p -> IntRes (eval_prefix p fcns scope)
+  | Postfix p -> IntRes (eval_postfix p fcns scope)
+and eval_int_expr expr fcns scope = match eval_expr expr fcns scope with
+  | IntRes i -> i
+  | VoidRes -> failwith @@ "Expression requires integer operand(s)."
 and eval_var var fcns scope = match scope with
   | [] -> failwith @@ var ^ " not declared."
   | h :: t ->
@@ -33,54 +36,59 @@ and eval_var var fcns scope = match scope with
       | Some s -> s
     else eval_var var fcns t
 and eval_value v = match v with (* TODO: support other types *)
-  | Integer i -> i
-  | Decimal d -> int_of_float d
-  | Letter l -> int_of_char l
+  | Integer i -> IntRes i
+  | Decimal d -> IntRes (int_of_float d)
+  | Letter l -> IntRes (int_of_char l)
 and eval_function_call (id, args) fcns scope =
   try
     let fcn = List.find (fun (_, f, _, _) -> String.compare f id = 0) fcns in
     let (return, params) = match fcn with (r, _, p, _) -> (r, p) in
     let table = Hashtbl.create hash_size in
     let process_arg arg (_, var) = 
-      let value = eval_expr arg fcns scope in
+      let value = eval_int_expr arg fcns scope in
       Hashtbl.add table var (Some value)
     in
     let () = List.iter2 process_arg args params in
     match eval_func fcn fcns [table] with
-    | RetRes i -> (match return with
-      | Void -> failwith @@ "Void function returned value."
-      | _ -> i)
-    | _ -> failwith "Function call did not return."
+    | RetRes (IntRes i) -> (match return with
+        | Void -> failwith @@ "Void function returned value."
+        | _ -> IntRes i)
+    | RetRes (VoidRes) -> (match return with
+        | Void -> VoidRes
+        | _ -> failwith @@ "Non-void function did not return value.")
+    | NoRes -> VoidRes
+    | _ -> failwith "Break or continue not inside loop or switch."
   with
   | Not_found -> failwith @@ id ^ " function not found."
   | Invalid_argument _ -> failwith @@ id ^ " invalid number of arguments."
 and eval_infix (e1, op, e2) fcns scope =
-  let lv = eval_expr e1 fcns scope in (* can evaluate this ahead of time *)
+  let eval e = eval_int_expr e fcns scope in
+  let lv = eval e1 in (* can evaluate this ahead of time *)
   match op with
-  | Plus -> lv + (eval_expr e2 fcns scope)
-  | Minus -> lv - (eval_expr e2 fcns scope)
-  | Times -> lv * (eval_expr e2 fcns scope)
-  | Divide -> lv / (eval_expr e2 fcns scope)
-  | Mod -> lv mod (eval_expr e2 fcns scope)
-  | ShiftLeft -> lv lsl (eval_expr e2 fcns scope)
-  | ShiftRight -> lv lsr (eval_expr e2 fcns scope)
-  | Less -> lv < (eval_expr e2 fcns scope) |> int_of_bool
-  | LesserEq -> lv <= (eval_expr e2 fcns scope) |> int_of_bool
-  | Greater -> lv > (eval_expr e2 fcns scope) |> int_of_bool
-  | GreaterEq -> lv >= (eval_expr e2 fcns scope) |> int_of_bool
-  | Equals -> lv == (eval_expr e2 fcns scope) |> int_of_bool
-  | NotEquals -> lv != (eval_expr e2 fcns scope) |> int_of_bool
-  | BitAnd -> lv land (eval_expr e2 fcns scope)
-  | BitXor -> lv lxor (eval_expr e2 fcns scope)
-  | BitOr -> lv lor (eval_expr e2 fcns scope)
+  | Plus -> lv + (eval e2)
+  | Minus -> lv - (eval e2)
+  | Times -> lv * (eval e2)
+  | Divide -> lv / (eval e2)
+  | Mod -> lv mod (eval e2)
+  | ShiftLeft -> lv lsl (eval e2)
+  | ShiftRight -> lv lsr (eval e2)
+  | Less -> lv < (eval e2) |> int_of_bool
+  | LesserEq -> lv <= (eval e2) |> int_of_bool
+  | Greater -> lv > (eval e2) |> int_of_bool
+  | GreaterEq -> lv >= (eval e2) |> int_of_bool
+  | Equals -> lv == (eval e2) |> int_of_bool
+  | NotEquals -> lv != (eval e2) |> int_of_bool
+  | BitAnd -> lv land (eval e2)
+  | BitXor -> lv lxor (eval e2)
+  | BitOr -> lv lor (eval e2)
   | And ->
-    (bool_of_int lv && bool_of_int (eval_expr e2 fcns scope)) |> int_of_bool
+    (bool_of_int lv && bool_of_int (eval e2)) |> int_of_bool
   | Or ->
-    (bool_of_int lv || bool_of_int (eval_expr e2 fcns scope)) |> int_of_bool
-  | Comma -> eval_expr e2 fcns scope
+    (bool_of_int lv || bool_of_int (eval e2)) |> int_of_bool
+  | Comma -> eval e2
 and eval_assign (id, op, e) fcns scope =
   try
-    let rhs = eval_expr e fcns scope in
+    let rhs = eval_int_expr e fcns scope in
     let res = match op with
       | Asgmt -> rhs
       | PlusA -> rhs + (eval_var id fcns scope)
@@ -94,11 +102,12 @@ and eval_assign (id, op, e) fcns scope =
       | BitOrA -> rhs lor (eval_var id fcns scope)
       | BitXorA -> rhs lxor (eval_var id fcns scope)
     in
-    let table = List.find (fun a -> Hashtbl.mem a id) scope
-    in Hashtbl.replace table id (Some res); res
+    let table = List.find (fun a -> Hashtbl.mem a id) scope in 
+    let () = Hashtbl.replace table id (Some res) in
+    res
   with Not_found -> failwith @@ id ^ " not declared."
 and eval_prefix (op, e) fcns scope =
-  let v = eval_expr e fcns scope in
+  let v = eval_int_expr e fcns scope in
   match op with
   | Incrmt -> eval_incr e fcns scope; v + 1
   | Decrmt -> eval_decr e fcns scope; v - 1
@@ -107,20 +116,20 @@ and eval_prefix (op, e) fcns scope =
   | Pos -> v
   | Neg -> -v
 and eval_postfix (e, op) fcns scope =
-  let v = eval_expr e fcns scope in
+  let v = eval_int_expr e fcns scope in
   match op with
   | Incrmt -> eval_incr e fcns scope; v
   | Decrmt -> eval_decr e fcns scope; v
   | _ -> failwith "Invalid postfix operator."
 and eval_incr e fcns scope = match e with
   | Var v ->
-    let curr = eval_expr e fcns scope + 1 in
+    let curr = eval_var v fcns scope + 1 in
     let table = List.find (fun a -> Hashtbl.mem a v) scope in
     Hashtbl.replace table v (Some curr)
   | _ -> failwith "Increment requires variable."
 and eval_decr e fcns scope = match e with
   | Var v ->
-    let curr = eval_expr e fcns scope - 1 in
+    let curr = eval_var v fcns scope - 1 in
     let table = List.find (fun a -> Hashtbl.mem a v) scope in
     Hashtbl.replace table v (Some curr)
   | _ -> failwith "Decrement requires variable."
@@ -139,7 +148,7 @@ and eval_dec decs fcns scope = match decs with
     | Assign (v, Asgmt, decs) ->
       if Hashtbl.mem table v then failwith @@ v ^ " is already declared."
       else
-        Hashtbl.add table v (Some (eval_expr decs fcns scope));
+        Hashtbl.add table v (Some (eval_int_expr decs fcns scope));
         eval_dec t fcns scope
     | _ -> failwith "Invalid declaration expression."
 
@@ -151,7 +160,7 @@ and eval_statements statements fcns scope = match statements with
 and eval_statement statement fcns scope = match statement with
   | Dec (p, decs) -> eval_dec decs fcns scope
   | Expr e -> let _ = eval_expr e fcns scope in NoRes
-  | Return -> NoRes
+  | Return -> RetRes (VoidRes)
   | ReturnExpr e -> RetRes (eval_expr e fcns scope)
   | Break -> BrkRes
   | Continue -> ContRes
@@ -164,7 +173,7 @@ and eval_statement statement fcns scope = match statement with
   | If i -> eval_if i fcns scope
   | IfElse i -> eval_if_else i fcns scope
 and eval_while (cond, statement) fcns scope =
-  if eval_expr cond fcns scope != 0 then
+  if eval_int_expr cond fcns scope != 0 then
     match eval_statement statement fcns scope with
     | NoRes -> eval_while (cond, statement) fcns scope
     | ContRes -> eval_while (cond, statement) fcns scope
@@ -173,7 +182,7 @@ and eval_while (cond, statement) fcns scope =
   else
     NoRes
 and eval_for cond inc statement fcns scope =
-  if eval_expr cond fcns scope != 0 then
+  if eval_int_expr cond fcns scope != 0 then
     match eval_statement statement fcns scope  with
     | NoRes ->
       let _ = eval_expr inc fcns scope in
@@ -187,13 +196,13 @@ and eval_for cond inc statement fcns scope =
     NoRes
 and eval_if (cond, statement) fcns scope =
   let new_scope = Hashtbl.create hash_size :: scope in
-  if eval_expr cond fcns scope != 0 then
+  if eval_int_expr cond fcns scope != 0 then
     eval_statement statement fcns new_scope
   else
     NoRes
 and eval_if_else (cond, s1, s2) fcns scope =
   let new_scope = Hashtbl.create hash_size :: scope in
-  let block = if eval_expr cond fcns scope != 0 then s1 else s2 in
+  let block = if eval_int_expr cond fcns scope != 0 then s1 else s2 in
   eval_statement block fcns new_scope
 
 (* TODO: process return type *)
@@ -213,6 +222,7 @@ let rec eval_prog prog =
 
 let _ =
   match Lexing.from_channel stdin |> Parser.prog Lexer.read |> eval_prog with
-  | RetRes v -> print_string @@ string_of_int v ^ "\n"
-  | NoRes -> failwith "Main did not return integer."
+  | RetRes (IntRes i) -> print_string @@ string_of_int i ^ "\n"
+  | RetRes (VoidRes) -> failwith "Main did not return integer."
+  | NoRes -> print_string "No result.\n"
   | _ -> failwith "Break or continue not inside loop or switch."
