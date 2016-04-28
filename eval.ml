e0f6d300f5c618953252d31811bf7a9b92e3226d
Brian Isganitis <brianji03@gmail.com>
2016-04-28 17:33:21 -0400

    Add global variables.

--- eval.ml
+++ eval.ml
@@ -14,27 +14,27 @@ let new_scope scope = Hashtbl.create hash_size :: scope
 let bool_of_int i = if i = 0 then false else true
 let int_of_bool b = if b then 1 else 0
 
-(* Get list of globals and functions that fcn can see out of the available
+(* Get list of globals and functions that elmt can see out of the available
    functions in prog. *)
-let rec prog_scope fcn prog = match prog with
+let rec prog_scope elmt prog = match prog with
   | [] -> failwith "Reached end of program scope without finding function."
-  | h :: t -> if h = fcn then [h] else h :: prog_scope fcn t
+  | h :: t -> if h = elmt then [h] else h :: prog_scope elmt t
 
 (* Functions for evaluating expressions. *)
-let rec eval_expr expr prog local = match expr with
+let rec eval_expr expr prog vars = match expr with
   | Empty -> failwith "Empty expression." (* TODO: for loop empty expression *)
-  | Var v -> eval_var v prog local
+  | Var v -> eval_var v prog vars
   | Value v -> eval_value v
-  | Paren e -> eval_expr e prog local
-  | FunctionCall f -> eval_function_call f prog local
-  | Infix i -> eval_infix i prog local
-  | Assign a -> eval_assign a prog local
-  | Prefix p -> eval_prefix p prog local
-  | Postfix p -> eval_postfix p prog local
-and eval_int_expr expr prog local = match eval_expr expr prog local with
+  | Paren e -> eval_expr e prog vars
+  | FunctionCall f -> eval_function_call f prog vars
+  | Infix i -> eval_infix i prog vars
+  | Assign a -> eval_assign a prog vars
+  | Prefix p -> eval_prefix p prog vars
+  | Postfix p -> eval_postfix p prog vars
+and eval_int_expr expr prog vars = match eval_expr expr prog vars with
   | Some i -> i
   | None -> failwith "Expected integer value."
-and eval_var var prog local = match local with
+and eval_var var prog vars = match vars with
   | [] -> failwith @@ var ^ " not declared."
   | h :: t ->
     if Hashtbl.mem h var then
@@ -50,8 +50,9 @@ and eval_value v =
     | CharVal l -> int_of_char l
   in
   Some num
-and eval_function_call (id, args) prog local =
+and eval_function_call (id, args) prog vars =
   try
+    (* Get function to be called *)
     let find_function f = match f with
       | Func (_, f, _, _) -> f = id
       | _ -> false
@@ -61,15 +62,24 @@ and eval_function_call (id, args) prog local =
       | Func f -> f
       | _ -> failwith "Found global instead of function."
     in
+
+    (* New scope for function *)
+    let rec get_globals = function
+      | [] -> failwith "Global scope missing."
+      | [t] -> t
+      | _ :: t -> get_globals t
+    in
     let fcn_prog = prog_scope fcn prog in
-    let table = Hashtbl.create hash_size in
+    let arg_vars = Hashtbl.create hash_size in
     let process_arg arg (_, var) =
-      let value = eval_int_expr arg prog local in
-      Hashtbl.add table var (Some value)
+      let value = eval_int_expr arg prog vars in
+      Hashtbl.add arg_vars var (Some value)
     in
     let () = List.iter2 process_arg args p in
-    let res = eval_statement b fcn_prog [table] in
-    match res with
+    let fcn_vars = [arg_vars; get_globals vars] in
+
+    (* Evaluate function and process return *)
+    match eval_statement b fcn_prog fcn_vars with
     | RetRes i -> (match r with
         | Void -> failwith "Void function returned value."
         | _ -> i)
@@ -81,8 +91,8 @@ and eval_function_call (id, args) prog local =
   with
   | Not_found -> failwith @@ id ^ " function not found."
   | Invalid_argument _ -> failwith @@ id ^ " invalid number of arguments."
-and eval_infix (e1, op, e2) prog local =
-  let eval e = eval_int_expr e prog local in
+and eval_infix (e1, op, e2) prog vars =
+  let eval e = eval_int_expr e prog vars in
   let num = match op with
     | Plus -> (eval e1) + (eval e2)
     | Minus -> (eval e1) - (eval e2)
@@ -102,13 +112,13 @@ and eval_infix (e1, op, e2) prog local =
     | BitOr -> (eval e1) lor (eval e2)
     | And -> (bool_of_int (eval e1) && bool_of_int (eval e2)) |> int_of_bool
     | Or -> (bool_of_int (eval e1) || bool_of_int (eval e2)) |> int_of_bool
-    | Comma -> let _ = eval_expr e1 prog local in eval e2
+    | Comma -> let _ = eval_expr e1 prog vars in eval e2
   in
   Some num
-and eval_assign (id, op, e) prog local =
+and eval_assign (id, op, e) prog vars =
   try
-    let rhs = eval_int_expr e prog local in
-    let eval id = eval_int_expr (Var id) prog local in
+    let rhs = eval_int_expr e prog vars in
+    let eval id = eval_int_expr (Var id) prog vars in
     let num = match op with
       | Asgmt -> rhs
       | PlusA -> rhs + (eval id)
@@ -122,66 +132,66 @@ and eval_assign (id, op, e) prog local =
       | BitOrA -> rhs lor (eval id)
       | BitXorA -> rhs lxor (eval id)
     in
-    let table = List.find (fun a -> Hashtbl.mem a id) local in
+    let table = List.find (fun a -> Hashtbl.mem a id) vars in
     let () = Hashtbl.replace table id (Some num) in
     Some num
   with Not_found -> failwith @@ id ^ " not declared."
-and eval_prefix (op, e) prog local =
-  let v = eval_int_expr e prog local in
+and eval_prefix (op, e) prog vars =
+  let v = eval_int_expr e prog vars in
   let num = match op with
-    | Incrmt -> let () = eval_incr e prog local in v + 1
-    | Decrmt -> let () = eval_decr e prog local in v - 1
+    | Incrmt -> let () = eval_incr e prog vars in v + 1
+    | Decrmt -> let () = eval_decr e prog vars in v - 1
     | Not -> bool_of_int v |> not |> int_of_bool
     | Comp -> lnot v
     | Pos -> v
     | Neg -> -v
   in
   Some num
-and eval_postfix (e, op) prog local =
-  let v = eval_int_expr e prog local in
+and eval_postfix (e, op) prog vars =
+  let v = eval_int_expr e prog vars in
   let () = match op with
-    | Incrmt -> eval_incr e prog local
-    | Decrmt -> eval_decr e prog local
+    | Incrmt -> eval_incr e prog vars
+    | Decrmt -> eval_decr e prog vars
     | _ -> failwith "Invalid postfix operator."
   in
   Some v
-and eval_incr e prog local = match e with
+and eval_incr e prog vars = match e with
   | Var v as var ->
-    let next = eval_int_expr var prog local + 1 in
-    let table = List.find (fun a -> Hashtbl.mem a v) local in
+    let next = eval_int_expr var prog vars + 1 in
+    let table = List.find (fun a -> Hashtbl.mem a v) vars in
     Hashtbl.replace table v (Some next)
   | _ -> failwith "Increment requires variable."
-and eval_decr e prog local = match e with
+and eval_decr e prog vars = match e with
   | Var v as var ->
-    let next = eval_int_expr var prog local - 1 in
-    let table = List.find (fun a -> Hashtbl.mem a v) local in
+    let next = eval_int_expr var prog vars - 1 in
+    let table = List.find (fun a -> Hashtbl.mem a v) vars in
     Hashtbl.replace table v (Some next)
   | _ -> failwith "Decrement requires variable."
 
 (* Evaluates statements. *)
-and eval_statements statements prog local = match statements with
+and eval_statements statements prog vars = match statements with
   | [] -> VoidRes
-  | h :: t -> match eval_statement h prog local with
-    | VoidRes -> eval_statements t prog local
+  | h :: t -> match eval_statement h prog vars with
+    | VoidRes -> eval_statements t prog vars
     | other -> other
-and eval_statement statement prog local = match statement with
-  | Dec (p, decs) -> eval_dec decs prog local
-  | Expr e -> let _ = eval_expr e prog local in VoidRes
+and eval_statement statement prog vars = match statement with
+  | Dec (p, decs) -> eval_dec decs prog vars
+  | Expr e -> let _ = eval_expr e prog vars in VoidRes
   | Return -> RetRes None
-  | ReturnExpr e -> RetRes (eval_expr e prog local)
+  | ReturnExpr e -> RetRes (eval_expr e prog vars)
   | Break -> BrkRes
   | Continue -> ContRes
-  | Block b -> eval_statements b prog (new_scope local)
-  | While w -> eval_while w prog (new_scope local)
+  | Block b -> eval_statements b prog (new_scope vars)
+  | While w -> eval_while w prog (new_scope vars)
   | For ((e1, e2, e3), s) ->
-    let _ = eval_expr e1 prog local in
-    eval_for e2 e3 s prog (new_scope local)
-  | If i -> eval_if i prog (new_scope local)
-  | IfElse i -> eval_if_else i prog (new_scope local)
-and eval_dec decs prog local = match decs with
+    let _ = eval_expr e1 prog vars in
+    eval_for e2 e3 s prog (new_scope vars)
+  | If i -> eval_if i prog (new_scope vars)
+  | IfElse i -> eval_if_else i prog (new_scope vars)
+and eval_dec decs prog vars = match decs with
   | [] -> VoidRes
   | h :: t ->
-    let table = match local with
+    let table = match vars with
       | [] -> failwith "Scope empty."
       | h :: _ -> h
     in
@@ -191,44 +201,56 @@ and eval_dec decs prog local = match decs with
         else v, None
       | Assign (v, Asgmt, decs) ->
         if Hashtbl.mem table v then failwith @@ v ^ " is already declared."
-        else v, Some (eval_int_expr decs prog local)
+        else v, Some (eval_int_expr decs prog vars)
       | _ -> failwith "Invalid declaration expression."
     in
     let () = Hashtbl.add table var value in
-    eval_dec t prog local
-and eval_while (cond, statement) prog local =
-  if eval_int_expr cond prog local != 0 then
-    match eval_statement statement prog local with
-    | VoidRes -> eval_while (cond, statement) prog local
-    | ContRes -> eval_while (cond, statement) prog local
+    eval_dec t prog vars
+and eval_while (cond, statement) prog vars =
+  if eval_int_expr cond prog vars != 0 then
+    match eval_statement statement prog vars with
+    | VoidRes -> eval_while (cond, statement) prog vars
+    | ContRes -> eval_while (cond, statement) prog vars
     | BrkRes -> VoidRes
     | other -> other
   else
     VoidRes
-and eval_for cond inc statement prog local =
-  if eval_int_expr cond prog local != 0 then
-    match eval_statement statement prog local  with
+and eval_for cond inc statement prog vars =
+  if eval_int_expr cond prog vars != 0 then
+    match eval_statement statement prog vars  with
     | VoidRes ->
-      let _ = eval_expr inc prog local in
-      eval_for cond inc statement prog local
+      let _ = eval_expr inc prog vars in
+      eval_for cond inc statement prog vars
     | ContRes ->
-      let _ = eval_expr inc prog local in
-      eval_for cond inc statement prog local
+      let _ = eval_expr inc prog vars in
+      eval_for cond inc statement prog vars
     | BrkRes -> VoidRes
     | other -> other
   else
     VoidRes
-and eval_if (cond, statement) prog local =
-  if eval_int_expr cond prog local != 0 then
-    eval_statement statement prog local
+and eval_if (cond, statement) prog vars =
+  if eval_int_expr cond prog vars != 0 then
+    eval_statement statement prog vars
   else
     VoidRes
-and eval_if_else (cond, s1, s2) prog local =
-  let block = if eval_int_expr cond prog local != 0 then s1 else s2 in
-  eval_statement block prog local
+and eval_if_else (cond, s1, s2) prog vars =
+  let block = if eval_int_expr cond prog vars != 0 then s1 else s2 in
+  eval_statement block prog vars
 
-let rec eval_main prog =
-  match eval_function_call ("main", []) prog [] with
+(* Process global variables *)
+let rec global_scope prog elmts vars = match elmts with
+  | [] -> ()
+  | h :: t -> match h with
+    | Global (p, decs) as g ->
+      let global_prog = prog_scope g prog in
+      let _ = eval_dec decs global_prog vars in
+      global_scope prog t vars
+    | _ -> global_scope prog t vars
+
+let eval_main prog =
+  let vars = [Hashtbl.create hash_size] in
+  let () = global_scope prog prog vars in
+  match eval_function_call ("main", []) prog vars with
   | Some i -> i
   | _ -> 0
 
